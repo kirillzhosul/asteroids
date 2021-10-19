251f2a3493077718a81832c0b4f7889266a33835
kirillzhosul <kodengprivate@gmail.com>
2021-10-19 23:23:25 +0400

    Player movement (Physics)

---
+++ src/Asteroids/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Speed for thrusting.
+    [SerializeField]
+    private float _thrustSpeed = 1.0f;
+
+    // Speed for rotation.
+    [SerializeField]
+    private float _rotateSpeed = 1.0f;
+
+    // Is we thrusting or not.
+    private bool _isThrusting = false;
+
+    // Direction in which we rotation.
+    private float _rotateDirection = 0.0f;
+
+    // Our player rigidbody.
+    private Rigidbody2D _rigidbody = null;
+
+    /// <summary>
+    /// Initialising physics at awakening.
+    /// </summary>
+    private void Awake()
+    {
+        // Grab our rigidbody.
+        this._rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Updating our controls.
+    /// </summary>
+    private void Update()
+    {
+        // Update is we thrusting or not.
+        this._isThrusting = (Input.GetAxis("Vertical") > 0);
+
+        // Update our rotate direction.
+        this._rotateDirection = Input.GetAxis("Horizontal");
+    }
+
+    /// <summary>
+    /// Updating our physics.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if (this._isThrusting)
+        {
+            // If we are thrusting.
+
+            // Add force.
+            _rigidbody.AddForce(this.transform.up * this._thrustSpeed);
+        }
+
+        if (this._rotateDirection != 0.0f)
+        {
+            // If we are turning in any direction.
+
+            // Add torque.
+            _rigidbody.AddTorque(this._rotateDirection * -1 * this._rotateSpeed);
+        }
+        
+    }
+
+}
